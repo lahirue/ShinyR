b9bf63d17c5756891338d00849b621db667e971b
lahirue <ekanayakelahiru@gmail.com>
2019-12-11 11:08:52 +0530

    adding uop changes

--- server.R
+++ server.R
@@ -1,25 +1,185 @@
-# example from http://shiny.rstudio.com/gallery/kmeans-example.html
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
 
 library(shiny)
-
-palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
-
-# Define server logic required to generate and plot a random distribution
-shinyServer(function(input, output, session) {
-
-  # Combine the selected variables into a new data frame
-  selectedData <- reactive({
-    iris[, c(input$xcol, input$ycol)]
-  })
-
-  clusters <- reactive({
-    kmeans(selectedData(), input$clusters)
-  })
-
-  output$plot1 <- renderPlot({
-    par(mar=c(5.1, 4.1, 0, 1))
-    plot(selectedData(), col=clusters()$cluster, pch=20, cex=3)
-    points(clusters()$centers, pch=4, cex=4, lwd=4)
-  })
-
+library(ggplot2)
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+    
+     recVal <- reactiveValues()
+     recVal$res <-  numeric(0) #data.frame(xVal = NA)
+     recVal$itr <- 0
+     recVal$accProb <- 0
+     recVal$xValue <- 0
+     recVal$propVal <- 0
+     recVal$rejected <- FALSE
+    
+    
+    ar <- reactive({
+        nIterations <- input$numeric
+        
+        if(recVal$itr < nIterations)
+        {
+            recVal$rejected <- TRUE
+            while(recVal$rejected)
+            {
+               
+                y <- runif(1) # g(y) ~ uniform(0,1)
+                
+                u <- runif(1)
+                
+                recVal$accProb <- dbeta(y,2,5)/2.4576
+                
+                recVal$propVal  <- y
+                
+                if(u < recVal$accProb) # c <- 2.4576 and g(y) = 1
+                {
+                    #points(y,u*2.4576, col="red")
+                    recVal$res <- c(recVal$res,y)
+                    recVal$xValue <- y
+                    recVal$rejected <- FALSE 
+                }                 
+            }
+            recVal$itr <- recVal$itr + 1
+            
+        }
+        #res
+    })
+    
+    
+    arProp <- reactive(
+        {
+            y <- runif(1) # g(y) ~ uniform(0,1)
+            
+            u <- runif(1)
+            
+            recVal$accProb <- dbeta(y,2,5)/2.4576
+            
+            recVal$propVal  <- y
+            
+            if(u < recVal$accProb) # c <- 2.4576 and g(y) = 1
+            {
+                #points(y,u*2.4576, col="red")
+                recVal$res <- c(recVal$res,y)
+                recVal$xValue <- y
+                recVal$rejected <- FALSE 
+            } 
+        }
+    )
+    
+   
+    
+    
+    # forward <- function()
+    # {
+    #     
+    # }
+    # 
+    
+    session<-reactiveValues()
+    session$timer<-reactiveTimer(Inf)
+    
+    observeEvent(input$start,{
+        session$timer<-reactiveTimer(300)
+        observeEvent(session$timer(),{
+            ar()
+        })
+    })
+    
+    
+    output$tDistPlot <- renderPlot(
+        {
+            p <- qplot(x, geom = "blank",ylab = "f(x)",xlab = "x") + theme_classic()
+            p <- p + geom_vline(xintercept = recVal$propVal, linetype="dotted", 
+                              color = "blue", size=1.5)
+            x <- seq(0,1,length.out = 100)
+            stat <- stat_function(aes(x = x, y = ..y..), fun = dbeta, colour="red", n = 100,
+                                  args = list(shape1 = 2, shape2 = 5))  
+             
+            return(p + stat )
+        }
+    )
+    
+    output$hist <- renderPlot({
+        
+        nIterations <- input$numeric
+        
+        # if(length(recVal$res)==0)
+        # {
+        #     p <- qplot(x, geom = "blank") + theme_classic()
+        #     
+        #     x <- seq(0,1,length.out = 100)
+        #     stat <- stat_function(aes(x = x, y = ..y..), fun = dbeta, colour="red", n = 100,
+        #                           args = list(shape1 = 2, shape2 = 5))   
+        #     stat <- stat 
+        #     return(p + stat)
+        #     
+        #     
+        #    
+        # 
+        # }
+        
+        
+        df <- data.frame(xVal = recVal$res)
+        
+        # x <- seq(0,1,length.out = length(recVal$res))
+        # stat <- stat_function(aes(x = x, y = ..y..), fun = dbeta, colour="red", n = 100,
+        #                       args = list(shape1 = 2, shape2 = 5)) 
+        # 
+        # 
+        
+        
+        
+        
+        
+        
+        #p <- ggplot(df, aes(x=xVal)) + geom_histogram(aes(y=..density..),bins = 30) + theme_classic()
+        p <- ggplot(df, aes(x=xVal)) + geom_histogram( bins = 30) + theme_classic()
+        
+        
+        
+        
+        
+        
+        #p + stat  + 
+        
+          p #+ stat  
+    })
+    
+    observeEvent(input$play,{
+          ar()
+    })
+    
+    observeEvent(input$stop,{
+        session$timer<-reactiveTimer(Inf)
+    })
+    
+    
+    observeEvent(input$reset,{
+        recVal$itr <- 0
+        recVal$res <-  numeric(0)
+    })
+    
+    output$Itr<-renderText({
+        paste("Iteration - ",recVal$itr,"" )
+    })
+    
+    output$accProb <-renderText({
+        paste("Acceptance Probability - ", round(recVal$accProb,2),"" )
+    }) 
+    
+    output$xVal <-renderText({
+        paste("x - ", round(recVal$xValue,2),"" )
+    }) 
+    
+        
+    observeEvent(input$nextItr,{
+        ar()
+    })
 })
--- ui.R
+++ ui.R
@@ -1,17 +1,65 @@
-# example from http://shiny.rstudio.com/gallery/kmeans-example.html
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
 
 library(shiny)
 
-shinyUI(
-  pageWithSidebar(
-    headerPanel('Iris k-means clustering'),
-    sidebarPanel(
-      selectInput('xcol', 'X Variable', names(iris)),
-      selectInput('ycol', 'Y Variable', names(iris), selected=names(iris)[[2]]),
-      numericInput('clusters', 'Cluster count', 3, min=1, max=9)
-    ),
-    mainPanel(
-      plotOutput('plot1')
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Acceptance -Rejection method"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("numeric",
+                        "Number of iterations:",
+                        min = 100,
+                        max = 10000,
+                        value = 500),
+            actionButton("start","Start"),
+            actionButton("stop","Stop"),
+            actionButton("nextItr","Next Iteration"),
+            
+            actionButton("reset","Reset") 
+            
+            #uiOutput("resetbutton")
+            
+        ),
+        
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            h4("Generating random numbers from Beta(2,5)"),
+            #plotOutput("tDistPlot"),
+            
+            fluidRow(
+                column(4,textOutput("Itr")),
+                column(4,textOutput("xVal")),
+                column(4,textOutput("accProb")) 
+                
+                
+            ),
+            
+        
+            
+            fluidRow(
+                
+                column(6,
+                       
+                       plotOutput('tDistPlot')
+                ),
+                column(6,
+                       plotOutput('hist')  
+                )
+                
+            )
+        )
     )
-  )
-)
+))
